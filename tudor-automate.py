9ab685f23fbf568b99f68422087cfbf258bd23ac
Bo Fethe <bofethe@gmail.com>
2024-04-08 18:42:41 -0400

    updated logger

--- tudor-automate.py
+++ tudor-automate.py
@@ -2,14 +2,15 @@ import requests
 import logging
 
 #Setup the loggers
-logging.getLogger("requests").setLevel(logging.WARNING)
-logging.getLogger("urllib3").setLevel(logging.WARNING)
 logger = logging.getLogger(__name__)
 logging.basicConfig(filename='output.log', 
                     level=logging.DEBUG, 
                     format='%(levelname)s | %(asctime)s | %(message)s')
+logging.getLogger("requests").setLevel(logging.WARNING)
+logging.getLogger("urllib3").setLevel(logging.WARNING)
 
 #Make the API call to vote
+logger.info('Trying to make API call')
 url = 'https://tools.gardenandgunmag.com/vote.php?env=live&s=192112&id=15&vt=bracket&v1=bracket000&v2=9d59ddca33cfe733dbadee24fdc0b620&cv=y&os=24&r=5&_=1712425379624'
 r = requests.get(url)
 output = r.json()
